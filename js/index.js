966772d6a5a263b771bed8905613c87051c12f01
Mateusz Łukasik <manfred.mateo@wp.pl>
2019-10-30 00:11:58 +0100

    dropdown visible

---
+++ js/index.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function (e) {
+    console.log('loaded');
+
+//    DROPDOWN MENU W KALKULATORZE//
+
+    var dropBtn = document.querySelector('.form_fields_btn');
+    var dropMenu = document.querySelector('.dropdown');
+
+    // console.log(dropMenu);
+    dropBtn.addEventListener('click', function(e){
+
+
+
+    })
+
+
+
+
+});
